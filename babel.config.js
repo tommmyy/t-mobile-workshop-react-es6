a155ed511e766c10dec483cb10fd42fde570d8f4
Tomáš Konrády <tomaskonrady@gmail.com>
2018-09-30 00:00:32 +0200

    adding webpack

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,10 @@ module.exports = {
 					firefox: '60',
 					chrome: '67',
 					safari: '11.1'
-				}
+				},
+				// https://babeljs.io/docs/en/next/babel-preset-env.html#modules
+				modules: false,
+				useBuiltIns: 'usage'
 			}
 		]
 	]
--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-const hello = () => 'Hello';
+(function(namespace, global) {
+	var DOMUtils = {
+		renderToElement: function(el, render, state) {
+			el.innerHTML = render(state);
+		}
+	};
 
-Promise.resolve().then(hello);
+	var StringUtils = {
+		toUpperFirst: function(x) {
+			return x.charAt(0).toUpperCase() + x.slice(1);
+		}
+	};
+
+	var rootElement = document.getElementById('root');
+
+	function createState(initialState) {
+		var state = initialState || {
+			users: []
+		};
+
+		return {
+			addUser: function addUser(user) {
+				var users = state.users.slice(); // copy of an array
+				users.push(user);
+
+				state.users = users;
+			},
+			getUsers: function getUsers() {
+				return state.users;
+			}
+		};
+	}
+
+	var AppState = createState();
+
+	function renderUserList(users) {
+		return (
+			'<ul>' +
+			users.map(function(user) {
+				return (
+					'<li>' +
+					StringUtils.toUpperFirst(user.name) +
+					' ' +
+					StringUtils.toUpperFirst(user.surname) +
+					'</li>'
+				);
+			}).join('') +
+			'</ul>'
+		);
+	}
+
+	function render() {
+		DOMUtils.renderToElement(rootElement, renderUserList, AppState.getUsers());
+	}
+
+	global[namespace] = {
+		addUser: AppState.addUser,
+		render: render
+	};
+})('APP', window);
+
+window.APP.addUser({
+	name: 'Jan',
+	surname: 'Novák'
+});
+
+window.APP.render();
