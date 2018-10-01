e00a13eb8cba9a9adc7883a04dbac007838999ed
Tomáš Konrády <tomaskonrady@gmail.com>
2018-10-01 13:16:50 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,67 +1,57 @@
 (function(namespace, global) {
-	var DOMUtils = {
-		renderToElement: function(el, render, state) {
+	const DOMUtils = {
+		renderToElement: (el, render, state) => {
 			el.innerHTML = render(state);
 		}
 	};
 
-	var StringUtils = {
-		toUpperFirst: function(x) {
-			return x.charAt(0).toUpperCase() + x.slice(1);
-		}
+	const StringUtils = {
+		toUpperFirst: (x) => x.charAt(0).toUpperCase() + x.slice(1)
 	};
 
-	var rootElement = document.getElementById('root');
+	const rootElement = document.getElementById('root');
 
-	function createState(initialState) {
-		var state = initialState || {
+	const createState = (initialState) => {
+		const state = initialState || {
 			users: []
 		};
 
 		return {
-			addUser: function addUser(user) {
-				var users = state.users.slice(); // copy of an array
-				users.push(user);
-
-				state.users = users;
+			addUser(user) {
+				state.users = [...state.users, user];
 			},
-			getUsers: function getUsers() {
+			getUsers() {
 				return state.users;
 			}
 		};
-	}
+	};
 
-	var AppState = createState();
+	const AppState = createState();
 
-	function renderUserList(users) {
-		return (
-			'<ul>' +
-			users.map(function(user) {
-				return (
-					'<li>' +
-					StringUtils.toUpperFirst(user.name) +
-					' ' +
-					StringUtils.toUpperFirst(user.surname) +
-					'</li>'
-				);
-			}).join('') +
-			'</ul>'
-		);
-	}
+	const capitalize = (strings, ...values) =>
+		strings.map(
+			(value, i) => `${value}${StringUtils.toUpperFirst(values[i] || '')}`
+		).join('');
 
-	function render() {
-		DOMUtils.renderToElement(rootElement, renderUserList, AppState.getUsers());
-	}
+	const renderUser = (user) => capitalize`<li>${user.name} ${user.surname}</li>`;
+
+	const renderUserList = (users) => `<ul>${users.map(renderUser).join('')}</ul>`;
+
+	const render = () => DOMUtils.renderToElement(
+		rootElement,
+		renderUserList,
+		AppState.getUsers()
+	);
 
 	global[namespace] = {
 		addUser: AppState.addUser,
-		render: render
+		render
 	};
 })('APP', window);
 
 window.APP.addUser({
-	name: 'Jan',
-	surname: 'Novák'
+	name: 'Jana',
+	surname: 'Nováková'
 });
 
 window.APP.render();
